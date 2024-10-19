99b65f6ff380060fa251906375c3462d0bcaf28f
Luca Pedrelli <pedrelli.luca@gmail.com>
2024-10-19 15:08:32 +0100

    Init VoxelTest repo

---
+++ Source/VoxelTest/VoxelTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class VoxelTest : ModuleRules
+{
+	public VoxelTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
