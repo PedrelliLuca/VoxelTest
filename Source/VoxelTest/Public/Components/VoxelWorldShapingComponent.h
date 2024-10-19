d0f2f63d720e97395d9cd5d0fb83956bbf87e67d
Luca Pedrelli <pedrelli.luca@gmail.com>
2024-10-19 15:31:25 +0100

    Private/Public folders, VoxelWorldShapingC

---
+++ Source/VoxelTest/Public/Components/VoxelWorldShapingComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Components/ActorComponent.h"
+#include "VoxelWorldShapingComponent.generated.h"
+
+/**
+ * \brief TODO
+ */
+UCLASS()
+class VOXELTEST_API UVoxelWorldShapingComponent : public UActorComponent {
+    GENERATED_BODY()
+
+public:
+    UVoxelWorldShapingComponent();
+};
